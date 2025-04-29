6dbc06317213e681792398100ccff32f40255e3d
Nathan <yeenathan21@gmail.com>
2025-04-28 17:01:00 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="p-8 flex flex-col items-center gap-4">
+      <h1 className='text-3xl'>SomaSync</h1>
+      <Button variant={"outline"}>Start</Button>
+    </div>
+  )
+}
+
+export default App
