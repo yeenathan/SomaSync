1a8a5fac5e040097b83edf55f9202832757e638a
Nathan <yeenathan21@gmail.com>
2025-05-13 14:40:54 -0700

    back button

--- src/sessions/sessionLayout.tsx
+++ src/sessions/sessionLayout.tsx
@@ -1,8 +1,9 @@
-import { Outlet, Link, useParams } from "react-router"; //children
+import { Outlet, Link, useParams, useNavigate, useLocation } from "react-router"; //children
 import NavigationRail from "@/components/ui/navigationRail";
 import SectionHead from "@/components/sectionHead";
 import { getPosts, getCategories, getCategoryNameFromID, getUserMeta } from "@/utils/WP";
 import { useEffect, useState } from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const posts = await getPosts();
 const categories = await getCategories();
@@ -11,6 +12,19 @@ function SessionLayout() {
   const params = useParams();
   const sessionid = params.sessionid;
   const [userProgress, setProgress] = useState(null);
+  const location = useLocation();
+
+  function getParentPath(pathname: string) {
+    const segments = pathname.split("/").filter(Boolean);
+    if (segments.length === 0) return "/";
+    if (segments[segments.length-2] === "chapter" || segments[segments.length-2] === "activity") {
+      segments.pop();
+      segments.pop();
+    } else {
+      segments.pop();
+    }
+    return "/" + segments.join("/");
+  }
 
   const header = sessionid?
     getCategoryNameFromID(posts.find((post:any) => post.slug.slice(0,8) === sessionid).categories[0], categories)
@@ -36,7 +50,10 @@ function SessionLayout() {
 
   return(
     <div className="p-8 min-w-full">
-      <header className="pb-4">
+      <header className="pb-4 flex flex-row gap-4 items-center">
+        <Link to={getParentPath(location.pathname)}>
+          <ArrowBackIcon/>
+        </Link>
         <Link to="/sessions" className="text-3xl"><h2>Sessions</h2></Link>
       </header>
       <div className="container flex flex-col mx-auto gap-8">
