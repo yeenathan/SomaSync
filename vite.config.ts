f12a4fb5bddace4e28c686086f8d0c1ebcfe7dc0
yeenathan <yeenathan21@gmail.com>
2025-05-21 15:46:57 -0700

    configuring build settings

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext"
+    }
+  },
+  build: {
+    target: "esnext"
+  },
 })
